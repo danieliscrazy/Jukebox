77f7e85194e3270d495224883d69e0914095bed4
danieliscrazy <176337916+danieliscrazy@users.noreply.github.com>
2025-05-25 10:31:35 -0400

    Update jukebox.py

--- jukebox.py
+++ jukebox.py
@@ -1,3 +1,6 @@
+# This code was partially made with the use of Copilot AI, specifically the functionality for detecting the removal of the NFC tag.
+# Given that most of the non-AI code was cobbled together from posts on Reddit, StackOverflow, and the Raspberry Pi forum, this code is exempted from the GNU GPLv3 that the rest of the repository is under. I take no credit or ownership of its contents, and you are free to do whatever you want with it.
+
 from time import sleep
 import sys
 import RPi.GPIO as GPIO
@@ -6,17 +9,25 @@ import vlc
 reader = SimpleMFRC522()
 
 p = vlc.MediaPlayer("file:///home/danield/jukebox/song1.mp3")
-                    
+
+last_id = None   
+
 try:
     while True:
         print("Hold a tag near the reader")
-        id, text = reader.read()
-        print(id)
-        sleep(5)
+        id, text = reader.read_no_block()
         if id == 584186575628:
-            print("Detected! Playing!")
-            p.play()
-            
+            if last_id != id:
+                print("Detected! Playing!")
+                p.play()
+            last_id = id
+        else:
+            if last_id is not None:
+                print("No longer detected! Stopped!")
+                p.stop()
+                last_id = None
+        sleep(0.2)
+
 except KeyboardInterrupt:
     GPIO.cleanup()
     
